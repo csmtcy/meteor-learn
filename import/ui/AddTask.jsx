de7acb8c5a367653735b73208b6245393ff0d4c8
mtcy <mtcy_9012@hotmail.com>
2016-09-05 14:56:29 +0800

    2nd

---
+++ import/ui/AddTask.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class AddTask extends Component {
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" placeholder="Enter task ... " />
+        </form>
+      </div>
+    );
+  }
+
+}
+
+export default AddTask;
